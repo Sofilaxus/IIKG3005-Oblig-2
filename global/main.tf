ab9c85e41fe054da2abaa9bec3c0436cdb6f3bd4
Sofia Serine <sofiasm@stud.ntnu.no>
2024-10-21 15:43:05 +0200

    updated main.tf global

--- global/main.tf
+++ global/main.tf
@@ -5,15 +5,9 @@ terraform {
       version = "3.116.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "rg_ssm_oblig2"
-    storage_account_name = "sassmoblig2"
-    container_name = "sc-sa-ssm-oblig2"
-    key = "ssm-oblig2-key"
-  }
-}
 
-provider "azurerm" {
+  provider "azurerm" {
     subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb"
   features {}
+}
 }
